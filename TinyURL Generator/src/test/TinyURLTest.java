ddb43709303ef7fefc4f289eeccc44c7afcf8855
Hou Teng Edward Lam <lamhoutenged@seattleu.edu>
2022-06-16 06:34:29 +0800

    TinyURLTest
    Changed TinyURLTest configuration from JUnit 5 to JUnit4

--- TinyURL Generator/src/test/TinyURLTest.java
+++ TinyURL Generator/src/test/TinyURLTest.java
@@ -1,15 +1,13 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-//import org.junit.jupiter.api.Test;
-
 import pkg.TinyURL;
 
-class TinyURLTest {
-	
+public class TinyURLTest {
+
 	@Test
 	public void addTinyURL() {
 		
@@ -18,8 +16,7 @@ class TinyURLTest {
 		String shortURL1 = t.longToShort("URL");
 		String shortURL2 = t.longToShort("URL2");
 		assertEquals(shortURL1, t.shortToLong("URL"));
-		assertEquals(shortURL2, t.shortToLong("URL"));
+		assertNotEquals(shortURL2, t.shortToLong("URL"));
 	}
 	
-
 }
