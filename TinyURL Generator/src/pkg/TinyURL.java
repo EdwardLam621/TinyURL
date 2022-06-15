27c57f1f18396656293e474b6c567c2660677b35
Hou Teng Edward Lam <lamhoutenged@seattleu.edu>
2022-06-16 05:49:41 +0800

    generateShortURL method added
    generateShortURL method added

---
+++ TinyURL Generator/src/pkg/TinyURL.java
@@ -0,0 +1,27 @@
+package pkg;
+
+import java.util.HashMap;
+
+public class TinyURL {
+    /*
+     * @param url: a long url
+     * @return: a short url starts with http://tiny.url/
+     */
+
+     private final String prefix = "http://tiny.url/";
+     private HashMap<String, String> longToShortMap = new HashMap<>();
+     private HashMap<String, String> shortToLongMap = new HashMap<>();
+     private String chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+     
+     public String longToShort (String url) {
+    	 
+     }
+     
+     public String ShortToLong (String url) {
+    	 
+     }
+     
+     private String generateShortURL() {
+    	 
+     }
+}
